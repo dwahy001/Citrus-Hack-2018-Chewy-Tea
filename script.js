1b9805caeda8b9ce1a56d876b750e8fa727e5990
Daniel Wahyu <dwahy001@ucr.edu>
2018-04-22 12:56:12 -0700

    Chewy Tea Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function onSignIn(googleUser) {
+	var profile=googleUser.getBasicProfile();
+	$(".g-signin2").css("display", "none");
+	$(".postLogin").css("display", "block" );
+
+}
+
+function signOut() {
+	var auth2 = gapi.auth2.getAuthInstance();
+	auth2.signOut().then(function() {
+
+		alert("Sign out successful");
+
+
+		$(".g-signin2").css("display","block");
+		$(".postLogin").css("display", "none");
+
+	
+
+	})
+}
